36503a2e4c07de109f575db6689e8498e7f9e568
AvitechG <vmlkumar74@gmail.com>
2021-12-11 09:40:03 +1000

    LoginPage

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:newapptest/Pages/HomePage.dart';
+import 'package:newapptest/Pages/LoginPage.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class  extends StatelessWidget {
-  const ({ Key? key }) : super(key: key);
-
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+        fontFamily: GoogleFonts.lato().fontFamily,
+      ),
+      routes: {
+        "/": (context) => LoginPage(),
+        "/homepage": (context) => HomePage(),
+      },
     );
   }
 }
